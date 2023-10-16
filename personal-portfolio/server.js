834fb8bbb3cc60b2264f782b8718f9b2808eedc1
Dewald <dewaldfourie0808@gmail.com>
2023-10-16 14:16:09 +0200

    updated server and contact functionality

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-// server used to send send emails
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+app.use(bodyParser.json())
+
+app.get("/api", (reg, res) => {
+  res.json({ message: "Hello from server"})
+})
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
-  },
-});
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASS
+  }
+})
 
 contactEmail.verify((error) => {
   if (error) {
@@ -26,27 +29,35 @@ contactEmail.verify((error) => {
   } else {
     console.log("Ready to Send");
   }
-});
+})
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+app.post("/api/contact", bodyParser.urlencoded({ extended: false }), (req, res) => {
+  const name = req.body.firstName + " " + req.body.lastName;
   const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
+  const phone =  req.body.phone;
+  const message = req.body.message; 
   const mail = {
     from: name,
-    to: "********@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
+    to: process.env.EMAIL_ADDRESS,
+    subject: "Dewald's Website Form",
     html: `<p>Name: ${name}</p>
-          <p>Email: ${email}</p>
-          <p>Phone: ${phone}</p>
-          <p>Message: ${message}</p>`,
-  };
+    <p>Email: ${email}</p>
+    <p>Phone: ${phone}</p>
+    <p>Message: ${message}</p>
+    `
+  }
+
   contactEmail.sendMail(mail, (error) => {
     if (error) {
       res.json(error);
     } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
-  });
-});
+      res.json({ code: 200, status: "Message Sent"})
+    } 
+  })
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is online on port: ${PORT}`);
+})
+
--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -16,81 +16,81 @@ import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
 
-  return (
-    <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={javaS} alt="" />
-                                <h5>JavaScript</h5>
-                            </div>
-                            <div className="item">
-                                <img src={python} alt="" />
-                                <h5>Python</h5>
-                            </div>
-                            <div className="item">
-                                <img src={reactIcon} alt="" />
-                                <h5>React</h5>
-                            </div>
-                            <div className="item">
-                                <img src={html5} alt="" />
-                                <h5>HTML5</h5>
-                            </div>
-                            <div className="item">
-                                <img src={css} alt="" />
-                                <h5>CSS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={git} alt="" />
-                                <h5>Git</h5>
-                            </div>
-                            <div className="item">
-                                <img src={sql} alt="Imge" />
-                                <h5>SQL</h5>
-                            </div>
-                            <div className="item">
-                                <img src={bootstrap} alt="" />
-                                <h5>Bootstrap</h5>
-                            </div>
-                            <div className="item">
-                                <img src={java} alt="" />
-                                <h5>Java</h5>
-                            </div>
-                            <div className="item">
-                                <img src={api} alt="" />
-                                <h5>API's</h5>
-                            </div>
-                        </Carousel>
+    return (
+        <section className="skill" id="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>Skills</h2>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                            <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                                <div className="item">
+                                    <img src={javaS} alt="" />
+                                    <h5>JavaScript</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={python} alt="" />
+                                    <h5>Python</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={reactIcon} alt="" />
+                                    <h5>React</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={html5} alt="" />
+                                    <h5>HTML5</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={css} alt="" />
+                                    <h5>CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={git} alt="" />
+                                    <h5>Git</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={sql} alt="Imge" />
+                                    <h5>SQL</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={bootstrap} alt="" />
+                                    <h5>Bootstrap</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={java} alt="" />
+                                    <h5>Java</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={api} alt="" />
+                                    <h5>API's</h5>
+                                </div>
+                            </Carousel>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <img className="background-image-left" src={colorSharp} alt="" />
-    </section>
-  )
+            <img className="background-image-left" src={colorSharp} alt="" />
+        </section>
+    )
 }
