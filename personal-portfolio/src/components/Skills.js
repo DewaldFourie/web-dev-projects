ded846c99dee5b43a3c644af7d5c1843c26a9d03
Dewald <dewaldfourie0808@gmail.com>
2024-11-21 07:31:16 +0200

    updated banner and skills components

--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -11,30 +11,82 @@ import bootstrap from "../assets/img/bootstrap.png"
 import java from "../assets/img/java.png"
 import api from "../assets/img/api.png"
 
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-3d-carousel'
+import 'react-responsive-3d-carousel/dist/styles.css'
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
+
+
+    const items = [
+        <div className="item">
+            <img src={javaS} alt="" />
+            <h5>JavaScript</h5>
+        </div>,
+        <div className="item">
+            <img src={python} alt="" />
+            <h5>Python</h5>
+        </div>,
+        <div className="item">
+            <img src={reactIcon} alt="" />
+            <h5>React</h5>
+        </div>,
+        <div className="item">
+            <img src={html5} alt="" />
+            <h5>HTML5</h5>
+        </div>,
+        <div className="item">
+            <img src={css} alt="" />
+            <h5>CSS</h5>
+        </div>,
+        <div className="item">
+            <img src={git} alt="" />
+            <h5>Git</h5>
+        </div>,
+        <div className="item">
+            <img src={sql} alt="Imge" />
+            <h5>SQL</h5>
+        </div>,
+        <div className="item">
+            <img src={bootstrap} alt="" />
+            <h5>Bootstrap</h5>
+        </div>,
+        <div className="item">
+            <img src={java} alt="" />
+            <h5>Java</h5>
+        </div>,
+        <div className="item">
+            <img src={api} alt="" />
+            <h5>API's</h5>
+        </div>
+    ]
+
+    const [dimensions, setDimensions] = useState(getDimensions());
+
+    // Function to determine dimensions based on screen size
+    function getDimensions() {
+        if (window.innerWidth <= 768) {
+            return { width: 0.32, height: '150px' , numOfSlides: 3, containerWidth: '100%', showArrows: false}; // Mobile
+        } else if (window.innerWidth <= 1024) {
+            return { width: 0.18, height: '150px' , numOfSlides: 5, containerWidth: '100%', showArrows: true}; // Tablet
+        } else {
+            return { width: 0.191, height: '300px', numOfSlides: 5, containerWidth: '100%', showArrows: true}; // Desktop
         }
-    };
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+            setDimensions(getDimensions());
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); // Empty dependency array ensures this runs only on mount/unmount
 
     return (
         <section className="skill" id="skills">
@@ -44,47 +96,17 @@ export const Skills = () => {
                         <div className="skill-bx wow zoomIn">
                             <h2>Skills</h2>
                             <p>Just some of my Technical Skills and Abilities.<br></br>If you would like to find out more, visit my LinkedIn page.</p>
-                            <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                                <div className="item">
-                                    <img src={javaS} alt="" />
-                                    <h5>JavaScript</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={python} alt="" />
-                                    <h5>Python</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={reactIcon} alt="" />
-                                    <h5>React</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={html5} alt="" />
-                                    <h5>HTML5</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={css} alt="" />
-                                    <h5>CSS</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={git} alt="" />
-                                    <h5>Git</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={sql} alt="Imge" />
-                                    <h5>SQL</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={bootstrap} alt="" />
-                                    <h5>Bootstrap</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={java} alt="" />
-                                    <h5>Java</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={api} alt="" />
-                                    <h5>API's</h5>
-                                </div>
+                            <Carousel className="carousel" items={items} 
+                                        startIndex={0} 
+                                        containerWidth={dimensions.containerWidth}
+                                        width={dimensions.width}
+                                        height={dimensions.height}
+                                        showIndicators={false}
+                                        showStatus={false}
+                                        showArrows={dimensions.showArrows}
+                                        depthFactor={1}
+                                        defaultOption={{numOfSlides: dimensions.numOfSlides}}
+                            >
                             </Carousel>
                         </div>
                     </div>
